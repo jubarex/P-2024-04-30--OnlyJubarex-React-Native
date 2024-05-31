6a88eaa9f8c66e4c520284baf95e683e335b2aed
jubarex <juliano@notari.com.br>
2024-05-31 17:03:16 -0300

    feature: salvar na galeria

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,27 +1,39 @@
-import React, { useRef } from 'react';
-import { View, Text, Button } from 'react-native';
-import { RNCamera } from 'react-native-camera';
-const CameraScreen = () => {
-const cameraRef = useRef<RNCamera | null>(null);
-const takePicture = async () => {
-if (cameraRef.current) {
-const options = { quality: 0.5, base64: true };
-const data = await cameraRef.current.takePictureAsync(options);
-console.log(data.uri); // URI da imagem capturada
-// Salvar a imagem na galeria se necessário
-}
-};
+import React, { useEffect, useRef , useState} from "react";
+import { View, Text, Button } from "react-native";
+import { RNCamera } from "react-native-camera";
 
-return (
-<View style={{ flex: 1 }}>
-<RNCamera
-ref={cameraRef}
-style={{ flex: 1 }}
-captureAudio={false}
-type={RNCamera.Constants.Type.back}
-/>
-<Button title="Tirar Foto" onPress={takePicture} />
-</View>
-);
+import * as MediaLibrary from 'expo-media-library';
+
+const CameraScreen = () => {
+
+    useEffect(()=>{
+        MediaLibrary.requestPermissionsAsync()
+    },[])
+  
+   
+
+  const cameraRef = useRef<RNCamera | null>(null);
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 1, base64: true, exif:false };
+      const data = await cameraRef.current.takePictureAsync(options);
+      console.log(data.uri); // URI da imagem capturada
+
+      MediaLibrary.saveToLibraryAsync(data.uri)
+      // Salvar a imagem na galeria se necessário
+    }
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <RNCamera
+        ref={cameraRef}
+        style={{ flex: 1 }}
+        captureAudio={false}
+        type={RNCamera.Constants.Type.back}
+      />
+      <Button title="Tirar Foto" onPress={takePicture} />
+    </View>
+  );
 };
 export default CameraScreen;
